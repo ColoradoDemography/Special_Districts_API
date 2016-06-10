7bc7a2acce52a8250216931fe618e032fcee4592
Daniel Trone <royhobbstn@yahoo.com>
2016-06-10 17:57:05 +0000

    converted php to js

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var routes = require("./routes/routes.js")(app);
+
+var server = app.listen(4567, function() {
+    console.log("Listening on port %s...", server.address().port);
+});
